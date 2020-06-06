833213fdfdd3a39a4cd8e9ba9281b9d6b93e95be
rishi <rishitiwari517@gmail.com>
2020-06-06 17:56:59 +0530

    UI update

--- app/src/main/java/com/rishi/tic_tac_toe20/M_game.java
+++ app/src/main/java/com/rishi/tic_tac_toe20/M_game.java
@@ -1,8 +1,5 @@
 package com.rishi.tic_tac_toe20;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,9 +7,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -25,35 +26,36 @@ import javax.annotation.Nullable;
 
 public class M_game extends AppCompatActivity {
 
-    private ImageView o_o,o_1,o_2,f_o,f_1,f_2,s_o,s_1,s_2;
+    private ImageView o_o, o_1, o_2, f_o, f_1, f_2, s_o, s_1, s_2;
     private FirebaseFirestore firebasefirestore;
-    private String player="1";
+    private String player = "1";
     private String roomkey;
     private static final String TAG = "M_game";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_game);
-        Intent intent=getIntent();
-        firebasefirestore=FirebaseFirestore.getInstance();
-        String user_2=intent.getStringExtra("user_2");
-        if(user_2.equals(User.getName())){
-            player="2";
+        Intent intent = getIntent();
+        firebasefirestore = FirebaseFirestore.getInstance();
+        String user_2 = intent.getStringExtra("user_2");
+        if (user_2.equals(User.getName())) {
+            player = "2";
         }
-        roomkey=intent.getStringExtra("roomkey");
-        o_o=findViewById(R.id.o_o);
-        o_1=findViewById(R.id.o_1);
-        o_2=findViewById(R.id.o_2);
-        f_o=findViewById(R.id.f_o);
-        f_1=findViewById(R.id.f_1);
-        f_2=findViewById(R.id.f_2);
-        s_o=findViewById(R.id.s_o);
-        s_1=findViewById(R.id.s_1);
-        s_2=findViewById(R.id.s_2);
+        roomkey = intent.getStringExtra("roomkey");
+        o_o = findViewById(R.id.o_o);
+        o_1 = findViewById(R.id.o_1);
+        o_2 = findViewById(R.id.o_2);
+        f_o = findViewById(R.id.f_o);
+        f_1 = findViewById(R.id.f_1);
+        f_2 = findViewById(R.id.f_2);
+        s_o = findViewById(R.id.s_o);
+        s_1 = findViewById(R.id.s_1);
+        s_2 = findViewById(R.id.s_2);
         click_listener();
     }
 
-    void click_listener(){
+    void click_listener() {
         o_o.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -111,11 +113,38 @@ public class M_game extends AppCompatActivity {
         database_listener();
     }
 
-    void register_move(String position){
-        HashMap<String,Object> map=new HashMap<>();
-        map.put("player",player);
-        map.put("position",position);
-        firebasefirestore.collection("roomkey").document(roomkey).collection("moves")
+    void register_move(String position) {
+        final HashMap<String, Object> map = new HashMap<>();
+        map.put("player", player);
+        map.put("position", position);
+        final DocumentReference documentReference = firebasefirestore
+                .collection("roomkey").document(roomkey)
+                .collection("moves")
+                .document(position);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (!documentSnapshot.exists()) {
+                        documentReference.set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(M_game.this, "move updated", Toast.LENGTH_SHORT).show();
+                                    database_listener();
+                                } else {
+                                    Toast.makeText(M_game.this, "move not registered", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(M_game.this, "cannot register this move", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+       /* firebasefirestore.collection("roomkey").document(roomkey).collection("moves")
                 .document(position).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -126,22 +155,22 @@ public class M_game extends AppCompatActivity {
                     Toast.makeText(M_game.this, "move not registered", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        });*/
     }
 
-    void database_listener(){
+    void database_listener() {
         firebasefirestore.collection("roomkey").document(roomkey).collection("moves")
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if(queryDocumentSnapshots!=null){
-                            for(DocumentChange doc:queryDocumentSnapshots.getDocumentChanges()){
-                                if(doc.getType()== DocumentChange.Type.ADDED){
-                                    Log.d(TAG, "onEvent: "+"new move registered");
-                                    DocumentSnapshot document= doc.getDocument();
-                                    String plya=document.getString("player");
-                                    String pos=document.getString("position");
-                                    update_UI(plya,pos);
+                        if (queryDocumentSnapshots != null) {
+                            for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                                if (doc.getType() == DocumentChange.Type.ADDED) {
+                                    Log.d(TAG, "onEvent: " + "new move registered");
+                                    DocumentSnapshot document = doc.getDocument();
+                                    String plya = document.getString("player");
+                                    String pos = document.getString("position");
+                                    update_UI(plya, pos);
                                 }
                             }
                         }
@@ -149,9 +178,9 @@ public class M_game extends AppCompatActivity {
                 });
     }
 
-    void update_UI(String player,String position){
-        Log.d(TAG, "update_UI: "+"UI updated");
-        if(player.equals("1")){
+    void update_UI(String player, String position) {
+        Log.d(TAG, "update_UI: " + "UI updated");
+        if (player.equals("1")) {
             switch (position) {
                 case "o_o":
                     o_o.setImageResource(R.drawable.x);
@@ -181,7 +210,7 @@ public class M_game extends AppCompatActivity {
                     s_2.setImageResource(R.drawable.x);
                     break;
             }
-        }else{
+        } else {
             switch (position) {
                 case "o_o":
                     o_o.setImageResource(R.drawable.zero);
@@ -213,4 +242,5 @@ public class M_game extends AppCompatActivity {
             }
         }
     }
+
 }
